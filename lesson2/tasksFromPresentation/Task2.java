46856399055feb93743294923f13480e0c6236d6
Nickeron-dev <koshkin061969@gmail.com>
2021-06-28 16:09:32 +0300

    Solved second task from presentation and put .class files in classes package.

--- lesson2/tasksFromPresentation/Task2.java
+++ lesson2/tasksFromPresentation/Task2.java
@@ -1,31 +1,29 @@
 /*
         Required output:
 
-        0 0 0 0 0
-        1 1 1 1
-        2 2 2
-        3 3
-        4
+        0 1 2 3 4
+        0 1 2 3
+        0 1 2
+        0 1
+        0
 
 */
 
 package classes;
 
-public class Task1 {
+public class Task2 {
 
         public static void main(String[] args) {
 
                 int[] arr = {0, 1, 2, 3, 4};
 
                 for (int i = 0; i < arr.length; i++) {
-                        int printTimes = arr[arr.length - (i + 1)]; // last element
-
-                        for ( ; printTimes >= 0; printTimes--) {
-                                System.out.print(arr[i]);
+                        for (int j = 0; j < arr.length - i; j++) {
+                                System.out.print(arr[j]);
                         }
                         System.out.println();
-
                 }
+                
         }
 
 }

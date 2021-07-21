30ae664338a1f5498a5f2758e84ba829bb3a52fc
Nickeron-dev <koshkin061969@gmail.com>
2021-07-21 23:31:39 +0300

    Rewrote example from video 2.

--- lesson12(CollectionsAndGenerics)/ExamplesFromVideos/MyLinkedList/src/main/java/LinkedContainer.java
+++ lesson12(CollectionsAndGenerics)/ExamplesFromVideos/MyLinkedList/src/main/java/LinkedContainer.java
@@ -1,8 +1,18 @@
-public class LinkedContainer<E> implements Linked<E> {
+import java.util.Iterator;
+
+public class LinkedContainer<E> implements Linked<E>, Iterable<E> {
 
     private Node<E> fstNode;
     private Node<E> lstNode;
     private int size = 0;
+
+    public static void main(String[] args) {
+        Linked<String> stringLinked = new LinkedContainer<>();
+        stringLinked.addLast("abc");
+        stringLinked.addFirst("def");
+        System.out.println(stringLinked.size());
+        System.out.println(stringLinked.getElementByIndex(0));
+    }
 
     public LinkedContainer() {
         lstNode = new Node<E>(null, fstNode, null);
@@ -11,32 +21,94 @@ public class LinkedContainer<E> implements Linked<E> {
 
     @Override
     public void addLast(E e) {
-
+        Node<E> prev = lstNode;
+        prev.setCurrentElement(e);
+        lstNode = new Node(null, prev, null);
+        prev.setNextElement(lstNode);
+        size++;
     }
 
     @Override
     public void addFirst(E e) {
-
+        Node<E> next = fstNode;
+        next.setCurrentElement(e);
+        fstNode = new Node(null, null, next);
+        next.setPrevElement(fstNode);
+        size++;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public E getElementByIndex(int counter) {
-        return null;
+        Node<E> target = fstNode.getNextElement();
+        for (int i = 0; i < counter; i++) {
+            target = getNextElement(target);
+        }
+        return target.getCurrentElement();
     }
+
+    private Node<E> getNextElement(Node<E> current) {
+        return current.getNextElement();
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            int counter = 0;
+
+            @Override
+            public boolean hasNext() {
+                return counter < size();
+            }
+
+            @Override
+            public E next() {
+                return getElementByIndex(counter++);
+            }
+        };
+    }
+
+    
 
     private class Node<E> {
         private E currentElement;
         private Node<E> nextElement;
         private Node<E> prevElement;
 
-        private Node(E currentElement, Node<E> prevElement, Node<E> nextElement) {
+        Node(E currentElement, Node<E> prevElement, Node<E> nextElement) {
             this.currentElement = currentElement;
-            this.nextElement = prevElement;
+            this.nextElement = nextElement;
+            this.prevElement = prevElement;
+        }
+
+        public E getCurrentElement() {
+            return currentElement;
+        }
+
+        public void setCurrentElement(E currentElement) {
+            this.currentElement = currentElement;
+        }
+
+        public Node<E> getNextElement() {
+            return nextElement;
+        }
+
+        public void setNextElement(Node<E> nextElement) {
+            this.nextElement = nextElement;
+        }
+
+        public Node<E> getPrevElement() {
+            return prevElement;
+        }
+
+        public void setPrevElement(Node<E> prevElement) {
+            this.prevElement = prevElement;
         }
     }
+
+
 }

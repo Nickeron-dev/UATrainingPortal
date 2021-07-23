2b45bf1c567fbfdf2ebaf2632a95480bdddd79fe
Nickeron-dev <koshkin061969@gmail.com>
2021-07-23 14:32:39 +0300

    Rewrote my homework with TDD.

--- lesson13(Java8)/homework/src/main/java/Solutions.java
+++ lesson13(Java8)/homework/src/main/java/Solutions.java
@@ -3,29 +3,57 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Solutions {
+
     public static final int MULTIPLY_BY = 3;
+
     public static void main(String[] args) {
         int [] array = new int[]{9, 5, 0, 7, 1, 3, 0};
-        IntStream intStream = Arrays.stream(array);
 
-        OptionalDouble average = intStream.average();
-        System.out.println("1.Average: " + average.getAsDouble());
+        System.out.println("1.Average: " + average(array));
 
-        OptionalInt minValue = Arrays.stream(array).min();
-        int index = Arrays.stream(array).boxed().collect(Collectors.toList()).indexOf(minValue.getAsInt());
-        System.out.println("2.Min: " + minValue.getAsInt() + " with index: " + index);
+        System.out.println("2.Min: " + minValue(array) + " with index: " + getIndex(array, minValue(array)));
 
-        int zeroValuesAmount = (int) Arrays.stream(array).filter((value) -> value == 0).count();
-        System.out.println("3.Amount of values equal to 0: " + zeroValuesAmount);
+        System.out.println("3.Amount of values equal to 0: " + zeroValuesAmount(array));
 
-        int valuesLargetThanZero = (int) Arrays.stream(array).filter((value) -> value > 0).count();
-        System.out.println("4.Amount of values larget than 0: " + valuesLargetThanZero);
+        System.out.println("4.Amount of values larget than 0: " + valuesLargerThanZero(array));
 
-        System.out.print("5.Multiply each element by 3: {");
-        Arrays.stream(array).forEach((value) -> {
-            System.out.print(" " + value * MULTIPLY_BY + " ");
-        });
-        System.out.print("}");
+        System.out.print("5.Multiply each element by 3: {" + multiplyArrayByValue(array, MULTIPLY_BY) + "}");
     }
 
+
+    public static double average(int[] array) {
+        IntStream intStream = Arrays.stream(array);
+        OptionalDouble average = intStream.average();
+        return average.getAsDouble();
+    }
+
+
+    public static int minValue(int[] array) {
+        OptionalInt minValue = Arrays.stream(array).min();
+        return minValue.getAsInt();
+    }
+
+    public static int getIndex(int[] array, int element) {
+        return Arrays.stream(array)
+                .boxed()
+                .collect(Collectors.toList())
+                .indexOf(element);
+    }
+
+    public static int zeroValuesAmount(int[] array) {
+        return (int) Arrays.stream(array).filter((value) -> value == 0).count();
+    }
+
+    public static int valuesLargerThanZero(int[] array) {
+        return (int) Arrays.stream(array).filter((value) -> value > 0).count();
+    }
+
+    public static String multiplyArrayByValue(int[] array, int number) {
+        StringBuffer temp = new StringBuffer("");
+        Arrays.stream(array).forEach((value) -> {
+            temp.append(" " + value * number + " ");
+        });
+        String result = new String(temp);
+        return result;
+    }
 }
--- lesson13(Java8)/homework/src/test/java/Testing.java
+++ lesson13(Java8)/homework/src/test/java/Testing.java
@@ -4,11 +4,35 @@ import org.junit.Test;
 import java.util.OptionalInt;
 
 public class Testing {
-//    @Test
-//    public void incrementTest() {
-//        int testValue = 5;
-//        Solutions.increment(testValue);
-//        Assert.assertEquals(6, testValue);
-//    }
+    int [] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    @Test
+    public void averageTest() {
+        Assert.assertEquals(5.0, (double) Solutions.average(array), 1.0);
+    }
+
+    @Test
+    public void minValueTest() {
+        Assert.assertEquals(0, Solutions.minValue(array));
+    }
+
+    @Test
+    public void getIndexTest() {
+        Assert.assertEquals(9, Solutions.getIndex(array, 0));
+    }
+
+    @Test
+    public void zeroValuesAmountTest() {
+        Assert.assertEquals(1, Solutions.zeroValuesAmount(array));
+    }
+
+    @Test
+    public void valuesLargerThanZeroTest() {
+        Assert.assertEquals(9, Solutions.valuesLargerThanZero(array));
+    }
+
+    @Test
+    public void multiplyArrayByValueTest() {
+        Assert.assertEquals(" 5  10  15  20  25  30  35  40  45  0 ", Solutions.multiplyArrayByValue(array, 5));
+    }
 
 }
